d3e9a80e449199d20d132360108dac26895d6466
Nathalie Rivas <ngrm97@gmail.com>
2022-12-05 18:47:57 -0500

    :sparkles: add slider functionality
    slider functionality with js

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Movie from './Movie';
 import {MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
-const Row = ({title, fetchURL}) => {
+const Row = ({title, fetchURL, rowID}) => {
     const [movies, setMovies] = useState([]);
     
     useEffect(() => {
@@ -15,12 +15,12 @@ const Row = ({title, fetchURL}) => {
     },[fetchURL])
 
     const slideLeft = () => {
-      let slider = document.getElementById('slider');
+      let slider = document.getElementById('slider' + rowID);
       slider.scrollLeft = slider.scrollLeft - 500;
     }
 
     const slideRight = () => {
-      let slider = document.getElementById('slider');
+      let slider = document.getElementById('slider' + rowID);
       slider.scrollLeft = slider.scrollLeft + 500;
     }
 
@@ -34,7 +34,7 @@ const Row = ({title, fetchURL}) => {
           size={40} 
           className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
         />
-        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+        <div id={'slider' + rowID} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
